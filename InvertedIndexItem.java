d25ed9ae003f015d53883f1fdf2eb47c91cbcd5d
andizhang <andybug@hotmail.com>
2018-11-28 16:05:40 -0500

    Create InvertedIndexItem.java

---
+++ InvertedIndexItem.java
@@ -0,0 +1,9 @@
+public class InvertedIndexItem {
+    public int fileID;
+    public int count;
+
+    public InvertedIndexItem(int fileID, int count) {
+        this.fileID = fileID;
+        this.count = count;
+    }
+}
