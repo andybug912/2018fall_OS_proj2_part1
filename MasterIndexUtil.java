548193456e2964d98d77438ca82c3ff29645ce97
andizhang <andybug@hotmail.com>
2018-12-02 14:34:41 -0500

    delete helper thread

--- MasterIndexUtil.java
+++ MasterIndexUtil.java
@@ -24,7 +24,6 @@ public class MasterIndexUtil {
     public static int fileChunkSize = 1024 * 1024; // file chunk size in byte
     public static int maxRowOfChunk = 20000;   // file chunk size in row
     public static int defaultMaxNumOfMappers = 3;
-    public static int MAPPER = 1, REDUCER = 2;
 
     public static String helperInfoFileName = "server_list.txt";
     public static String reducerInfoFileName = "reducer_list.txt";
